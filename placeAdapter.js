e497b70c2f0b9f7c8095c833220b1cdf05028cbb
Randi Schreiner <69395829+mimirandi21@users.noreply.github.com>
2021-03-08 19:04:51 -0600

    Update placeAdapter.js

--- placeAdapter.js
+++ placeAdapter.js
@@ -1,6 +1,6 @@
 class PlaceAdapter {
 	static fetchPlaces() {
-		return fetch("https://obscure-depths-42428.herokuapp.com/places")
+		return fetch("https://travel-tips-updated.herokuapp.com//places")
 			.then((res) => res.json())
 			.then((placeInfo) => {
 				return placeInfo.forEach(function (place) {
